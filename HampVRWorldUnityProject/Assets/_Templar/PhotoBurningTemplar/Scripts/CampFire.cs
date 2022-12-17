7afe2ec7b4ea9a10636be957ae5ca98bac4bc620
Templar <carlegos101@gmail.com>
2022-12-17 01:40:48 -0600

    added pics, photo n poi

---
+++ HampVRWorldUnityProject/Assets/_Templar/PhotoBurningTemplar/Scripts/CampFire.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CampFire : UdonSharpBehaviour
+{
+    public Photo photo;
+    public MeshRenderer meshRenderer;
+    public void Start()
+    {
+        if (meshRenderer != null) meshRenderer.enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Something entered the trigger");
+        if(other != null)
+        {
+            if(other.gameObject.layer ==13)//is a pickup object
+            {
+                photo = other.GetComponent<Photo>();
+                photo.RespawnMultiplayer();
+            }
+        }
+    }
+}
---
+++ HampVRWorldUnityProject/Assets/_Templar/PhotoBurningTemplar/Scripts/Photo.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Photo : UdonSharpBehaviour
+{
+    public Transform OriginalPosition;
+    public VRCPlayerApi localplayer;
+    public VRC_Pickup myPickUp;
+    void Start()
+    {
+        if(OriginalPosition == null) OriginalPosition = GetComponentInParent<Transform>();
+        localplayer = Networking.LocalPlayer;
+    }
+    public void RespawnMultiplayer()
+    {
+        Debug.Log("Respawning");
+        myPickUp.Drop();
+        if (localplayer.isMaster)
+        {
+            Networking.SetOwner(localplayer, gameObject);
+            gameObject.transform.position = OriginalPosition.transform.position;
+        }   
+    }
+    
+
+
+}
