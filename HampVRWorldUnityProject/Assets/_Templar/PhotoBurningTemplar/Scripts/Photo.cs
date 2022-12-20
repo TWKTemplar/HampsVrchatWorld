246b55aee1ea6e24dfc6f94e0bd787f4815a2085
Templar <carlegos101@gmail.com>
2022-12-20 15:15:15 -0600

    Reworked Burning photo code and added cringe Paper

--- HampVRWorldUnityProject/Assets/_Templar/PhotoBurningTemplar/Scripts/Photo.cs
+++ HampVRWorldUnityProject/Assets/_Templar/PhotoBurningTemplar/Scripts/Photo.cs
@@ -9,39 +9,41 @@ public class Photo : UdonSharpBehaviour
     public Transform OriginalPosition;
     public VRCPlayerApi localplayer;
     public VRC_Pickup myPickUp;
-    public MeshRenderer meshRenderer;
-    public BurningPhoto BurningPhoto;
     public float BurnTime = 15;
+    public GameObject Effects;
+
+    public Animator animator;
     void Start()
     {
         if(OriginalPosition == null) OriginalPosition = GetComponentInParent<Transform>();
         localplayer = Networking.LocalPlayer;
-        if (meshRenderer == null) meshRenderer = GetComponent<MeshRenderer>();
+        if (animator == null) animator = GetComponent<Animator>();
     }
     public void DoBurn()
     {
-        Debug.Log("Burning");
         myPickUp.Drop();
         if (localplayer.isMaster)
         {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "ActivateBurningVersion");
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "StartBurningAnim");
             SendCustomEventDelayedSeconds("ResetObject", BurnTime);
         }   
     }
     public void ResetObject()//resets everything
     {
+        myPickUp.Drop();
         Networking.SetOwner(localplayer, gameObject);
         gameObject.transform.position = OriginalPosition.transform.position;
-        meshRenderer.enabled = true;
-        BurningPhoto.skinnedMeshRenderer.enabled = false;
-        BurningPhoto.ResetPhoto();
+        StopBurningAnim();
     }
-    public void ActivateBurningVersion()
+    public void StartBurningAnim()
     {
-        meshRenderer.enabled = false;
-        BurningPhoto.skinnedMeshRenderer.enabled = true;
-        BurningPhoto.skinnedMeshRenderer.materials = meshRenderer.materials;
-        BurningPhoto.StartBurning();
+        if(Effects != null) Effects.SetActive(true);
+        if (animator != null) animator.SetBool("IsBurning", true);
+    }
+    public void StopBurningAnim()
+    {
+        if (Effects != null) Effects.SetActive(false);
+        if (animator != null) animator.SetBool("IsBurning", false);
     }
 
 
