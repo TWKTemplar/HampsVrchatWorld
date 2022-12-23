804846521e97429d34a5b63687776ad28b440dc4
HampVR <hampton.moseley@gmail.com>
2022-12-23 14:55:07 -0600

    added toggleable mirror

---
+++ HampVRWorldUnityProject/Assets/_Scenes/waltuh_UdonProgramSources/MikeMirrorToggle.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorToggle : UdonSharpBehaviour
+{
+    public GameObject bestMirror;
+    public bool mirrorIsOn;
+
+    void Start()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        if (mirrorIsOn == false)
+        {
+            bestMirror.SetActive(true);
+            mirrorIsOn = true;
+        }
+        else
+        {
+            bestMirror.SetActive(false);
+            mirrorIsOn = false;
+        }
+    }
+}
