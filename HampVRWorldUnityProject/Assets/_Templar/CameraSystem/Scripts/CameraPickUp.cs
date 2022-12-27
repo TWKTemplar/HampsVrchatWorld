df9e7307b976d4cdcd76e373e6f30dc85a1651e7
Templar <carlegos101@gmail.com>
2022-12-27 14:00:45 -0600

    Added working Camera

---
+++ HampVRWorldUnityProject/Assets/_Templar/CameraSystem/Scripts/CameraPickUp.cs
@@ -0,0 +1,52 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CameraPickUp : UdonSharpBehaviour
+{
+    public Animator anim;
+    public VRC_Pickup pickup;
+    public SkinnedMeshRenderer myRend;
+    public VRCPlayerApi localplayer;
+    public float ImageBlendValue = 0;
+    public float ImageBlendSpeed = 10;
+    public float TimeSinceLastPhotoTaken = 0f;
+
+    public float TimeSinceLastPhotoTakenCoolDown = 5f;
+    public void Start()
+    {
+        localplayer = Networking.LocalPlayer;
+    }
+    public void Update()
+    {
+        TimeSinceLastPhotoTaken += Time.deltaTime;
+        LookAtCameraBack();
+        
+    }
+    public void LookAtCameraBack()
+    {
+        var dist = Vector3.Distance(localplayer.GetBonePosition(HumanBodyBones.Head), transform.position);
+        if(TimeSinceLastPhotoTaken < TimeSinceLastPhotoTakenCoolDown)
+        {
+            if(pickup.IsHeld) ImageBlendValue += Time.deltaTime * ImageBlendSpeed;
+        }
+        else
+        {
+            ImageBlendValue -= Time.deltaTime * ImageBlendSpeed;
+        }
+        ImageBlendValue = Mathf.Clamp(ImageBlendValue, 0, 100);
+        myRend.SetBlendShapeWeight(0, ImageBlendValue);// 0 to 100
+    }
+    public override void OnPickupUseDown()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "LocalTakePicture");
+    }
+    public void LocalTakePicture()
+    {
+        TimeSinceLastPhotoTaken = 0;
+        anim.Play("TakePic");
+    }
+
+}
---
+++ HampVRWorldUnityProject/Assets/_Templar/CameraSystem/Scripts/LightToggle.cs
@@ -0,0 +1,51 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LightToggle : UdonSharpBehaviour
+{
+    public SkinnedMeshRenderer rend;
+    public GameObject Light;
+    public bool IsLightsOn = true;
+    public void Start()
+    {
+        SyncAnimWithLight();
+    }
+    public override void Interact()
+    {
+        IsLightsOn = !IsLightsOn;//Flip the bool
+        if (IsLightsOn)
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "SetLightsOn");
+        }
+        else
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "SetLightsOff");
+        }
+    }
+    public void SetLightsOn()
+    {
+        IsLightsOn = true;
+        Light.SetActive(IsLightsOn);
+        SyncAnimWithLight();
+    }
+    public void SetLightsOff()
+    {
+        IsLightsOn = false;
+        Light.SetActive(IsLightsOn);
+        SyncAnimWithLight();
+    }
+    public void SyncAnimWithLight()
+    {
+        if (!IsLightsOn)
+        {
+            rend.SetBlendShapeWeight(1, 100);
+        }
+        else
+        {
+            rend.SetBlendShapeWeight(1, 0);
+        }
+    }
+}
