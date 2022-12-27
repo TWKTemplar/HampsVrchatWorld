03aa1c14dd26fc8600e9d47af830327e81fc1db8
Templar <carlegos101@gmail.com>
2022-12-27 14:21:10 -0600

    Added Working camera photo that burns Working Camera With Taken Photo

--- HampVRWorldUnityProject/Assets/_Templar/CameraSystem/Scripts/CameraPickUp.cs
+++ HampVRWorldUnityProject/Assets/_Templar/CameraSystem/Scripts/CameraPickUp.cs
@@ -13,8 +13,10 @@ public class CameraPickUp : UdonSharpBehaviour
     public float ImageBlendValue = 0;
     public float ImageBlendSpeed = 10;
     public float TimeSinceLastPhotoTaken = 0f;
-
     public float TimeSinceLastPhotoTakenCoolDown = 5f;
+    public GameObject CameraPhoto;
+    public Transform CameraPhotoSpawnPoint;
+
     public void Start()
     {
         localplayer = Networking.LocalPlayer;
@@ -42,6 +44,27 @@ public class CameraPickUp : UdonSharpBehaviour
     public override void OnPickupUseDown()
     {
         SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "LocalTakePicture");
+        SendCustomEventDelayedSeconds("SpawnPhoto", 0.5f);
+    }
+    public void SpawnPhoto()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "MasterMovePhoto");
+    }
+    public void MasterMovePhoto()
+    {
+
+        if (localplayer.isMaster)
+        {
+            if(CameraPhoto != null)
+            {
+                if(CameraPhotoSpawnPoint != null)
+                {
+                    Networking.SetOwner(localplayer, CameraPhoto);
+                    CameraPhoto.transform.position = CameraPhotoSpawnPoint.transform.position;
+                    CameraPhoto.transform.rotation = CameraPhotoSpawnPoint.transform.rotation;
+                }
+            }
+        }
     }
     public void LocalTakePicture()
     {
