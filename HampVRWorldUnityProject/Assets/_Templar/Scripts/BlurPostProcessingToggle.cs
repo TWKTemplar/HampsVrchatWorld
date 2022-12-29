095cf278919036f7bd9469abcead6d3c2024c69a
Templar <carlegos101@gmail.com>
2022-12-29 13:38:23 -0600

    Cleaned Up Map area, added DOF toggle

---
+++ HampVRWorldUnityProject/Assets/_Templar/Scripts/BlurPostProcessingToggle.cs
@@ -0,0 +1,45 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class BlurPostProcessingToggle : UdonSharpBehaviour
+{
+
+    public GameObject BlurEffectObject;
+    public Image color;
+    Color red = new Color(1, 0, 0);
+    Color green = new Color(0, 1, 0);
+    void Start()
+    {
+        if (BlurEffectObject == null) return;
+        if (BlurEffectObject.activeInHierarchy)
+        {
+            color.color = green;
+        }
+        else
+        {
+            color.color = red;
+        }
+    }
+
+    public override void Interact()
+    {
+        if (BlurEffectObject == null) return;
+        if (BlurEffectObject.activeInHierarchy)
+        {
+            color.color = red;
+            BlurEffectObject.SetActive(false);
+        }
+        else
+        {
+            color.color = green;
+            BlurEffectObject.SetActive(true);
+
+        }
+    }
+}
