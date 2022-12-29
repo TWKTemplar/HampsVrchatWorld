1f3f96bfb97a77a3fcecb55577918159dab36b9d
HampVR <hampton.moseley@gmail.com>
2022-12-29 12:44:54 -0600

    changed terrain and added fire flicker script

---
+++ HampVRWorldUnityProject/Assets/_Hamp/Scripts/flicker.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class flicker : UdonSharpBehaviour
+{
+
+    public Light fire;
+    private float intensityMin = 0.5f;
+    private float intensityMax = 1.5f;
+    private float frequency = 0.1f;
+    private float amplitude = 0.3f;
+
+    void Update()
+    {
+        float t = Time.time;
+        fire.intensity = Mathf.Lerp(intensityMin, intensityMax, Mathf.PerlinNoise(t * frequency, 0)) + Random.Range(-amplitude, amplitude);
+    }
+
+
+    //{
+
+    //    public Light fire;
+
+
+    //    void Update()
+
+    //    {
+    //        var x = Time.time;
+    //        fire.intensity = (Mathf.Sin(x) * 0.25f + 1f) + (Mathf.Sin(x * 5) * 0.5f + 0.5f); 
+
+
+    //    }
+
+    //}
+
+}
