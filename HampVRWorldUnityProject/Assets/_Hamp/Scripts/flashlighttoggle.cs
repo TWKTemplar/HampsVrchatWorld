b87113bae4206910f9a87b59b77059c1580823c6
Templar <carlegos101@gmail.com>
2023-01-03 18:44:38 -0600

    Added networking code to flash light

--- HampVRWorldUnityProject/Assets/_Hamp/Scripts/flashlighttoggle.cs
+++ HampVRWorldUnityProject/Assets/_Hamp/Scripts/flashlighttoggle.cs
@@ -8,12 +8,49 @@ public class flashlighttoggle : UdonSharpBehaviour
     
 {
     public GameObject flashlight;
+    public AudioSource audioSource;
     public override void OnPickupUseDown()
     {
-        flashlight.SetActive(!flashlight.activeInHierarchy);
+        ToggleFlashLight();
     }
-    void Start()
+    public void ToggleFlashLight()
     {
-        
+        if(audioSource != null) audioSource.Play();
+
+        if (flashlight.activeInHierarchy)
+        {
+            //TurnOffFlashlight();
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "TurnOffFlashlight");
+        }
+        else
+        {
+            //TurnOnFlashlight();
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "TurnOnFlashlight");
+        }
+    }
+    public void TurnOnFlashlight()
+    {
+        flashlight.SetActive(true);
+    }
+    public void TurnOffFlashlight()
+    {
+        flashlight.SetActive(false);
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (Networking.IsMaster)
+        {
+            if (flashlight.activeInHierarchy)
+            {   
+                //TurnOnFlashlight();
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "TurnOnFlashlight");
+            }
+            else
+            {
+                //TurnOffFlashlight();
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "TurnOffFlashlight");
+            }
+        }
     }
 }
