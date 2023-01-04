2da3ff695a536ecc7fafcac06e4731b8da92e9bd
HampVR <hampton.moseley@gmail.com>
2023-01-03 18:24:55 -0600

    added flashlight

---
+++ HampVRWorldUnityProject/Assets/_Hamp/Scripts/flashlighttoggle.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class flashlighttoggle : UdonSharpBehaviour
+    
+{
+    public GameObject flashlight;
+    public override void OnPickupUseDown()
+    {
+        flashlight.SetActive(!flashlight.activeInHierarchy);
+    }
+    void Start()
+    {
+        
+    }
+}
